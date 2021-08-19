66de4ebc4698203bfc8f278197f7f614295d39da
Nadeem <anwarnadeem2010@gmail.com>
2021-08-19 15:11:30 +0530

    half todo app

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import './App.css';
 import Nadeem from './component/Nadeem';
 import Counter from './component/Counter';
 import Game from './component/Game';
+import Todoapp from './component/Todoapp';
 
 
 function App() {
   return (
     <div className="App">
-    <Nadeem name="Anwar"/>,
-    
-    
-    <Counter/>,
-
+    <Nadeem name="Anwar"/>,   
+    <Counter/>
     <Game/>
 
-
+    <Todoapp/>
     </div>
   );
 }
